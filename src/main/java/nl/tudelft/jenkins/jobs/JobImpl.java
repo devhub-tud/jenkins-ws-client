b26669ed0c5b518aa58692b7592c930b073c993f
David Hartveld <david@hartveld.net>
2012-12-10 19:07:07 +0100

    Set limited permissions instead of full permissions.

--- src/main/java/nl/tudelft/jenkins/jobs/JobImpl.java
+++ src/main/java/nl/tudelft/jenkins/jobs/JobImpl.java
@@ -25,6 +25,12 @@ import org.slf4j.LoggerFactory;
 
 public class JobImpl implements Job {
 
+	/**
+	 * If true, Jobs are created with limited permissions, specifically tailored
+	 * for use with DevHub.
+	 */
+	private static final boolean DEVHUB = true;
+
 	private static final Logger LOG = LoggerFactory.getLogger(JobImpl.class);
 
 	private final String name;
@@ -41,7 +47,6 @@ public class JobImpl implements Job {
 	}
 
 	public JobImpl(final String name, final Document document) {
-
 		checkArgument(isNotEmpty(name), "name must be non-empty");
 
 		this.name = name;
@@ -49,7 +54,6 @@ public class JobImpl implements Job {
 
 		final Element element = findSingleElementInDocumentByXPath(document, XPATH_PROPERTIES_SECURITY);
 		permissionMatrix = JobPermissionMatrixImpl.fromElement(element);
-
 	}
 
 	@Override
@@ -59,13 +63,13 @@ public class JobImpl implements Job {
 
 	@Override
 	public void setScmUrl(final String scmUrl) {
+		LOG.trace("Setting SCM URL to: {}", scmUrl);
 
 		checkArgument(isNotEmpty(scmUrl), "scmUrl must be non-empty");
 
 		final Element url = findSingleElementInDocumentByXPath(document, XPATH_SCM_GIT_URL);
 
 		url.setContent(new Text(scmUrl));
-
 	}
 
 	@Override
@@ -80,9 +84,21 @@ public class JobImpl implements Job {
 
 	@Override
 	public void addUser(User user) {
+		LOG.trace("Adding user with full permissions: {}", user);
+
 		checkNotNull(user, "user");
 
-		addFullPermissionsForUser(user);
+		if (DEVHUB) {
+			addDevHubPermissionsForUser(user);
+		} else {
+			addFullPermissionsForUser(user);
+		}
+	}
+
+	private void addDevHubPermissionsForUser(User user) {
+		permissionMatrix.addPermission(user, JobAuthMatrixPermission.JOB_DISCOVER);
+		permissionMatrix.addPermission(user, JobAuthMatrixPermission.JOB_READ);
+		permissionMatrix.addPermission(user, JobAuthMatrixPermission.JOB_WORKSPACE);
 	}
 
 	private void addFullPermissionsForUser(User user) {
@@ -100,17 +116,18 @@ public class JobImpl implements Job {
 
 	@Override
 	public void setNotificationRecipient(final User recipient) {
+		LOG.trace("Setting single notification recipient: {}", recipient);
 
 		checkNotNull(recipient, "recipient must be non-null");
 
 		final Element recipients = findSingleElementInDocumentByXPath(document, XPATH_NOTIFICATION_RECIPIENTS);
 
 		recipients.setContent(new Text(recipient.getEmail()));
-
 	}
 
 	@Override
 	public void addNotificationRecipient(final User recipient) {
+		LOG.trace("Adding additional notification recipient: {}", recipient);
 
 		checkNotNull(recipient, "recipient must be non-null");
 
@@ -126,7 +143,6 @@ public class JobImpl implements Job {
 		} else {
 			throw new RuntimeException("Element on path " + XPATH_NOTIFICATION_RECIPIENTS + " contains multiple children. Single (text) element expected");
 		}
-
 	}
 
 	@Override
@@ -143,17 +159,14 @@ public class JobImpl implements Job {
 
 	@Override
 	public String toString() {
-
 		final ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE);
 
 		builder.append("name", name);
 
 		return builder.toString();
-
 	}
 
 	public static Job fromXml(final String name, final String xml) {
-
 		LOG.trace("Creating job named {} from xml ...", name);
 
 		checkArgument(isNotEmpty(name), "name must be non-empty");
@@ -164,7 +177,6 @@ public class JobImpl implements Job {
 		final Document document = XmlUtils.createJobDocumentFrom(is);
 
 		return new JobImpl(name, document);
-
 	}
 
 }
