b1dcdc6d63ff2d781cde1de67f26d1ffe6c2bb30
David Hartveld <david@hartveld.net>
2012-12-28 16:29:02 +0100

    Enabled and extended basic jenkins service test.

--- src/test/java/nl/tudelft/jenkins/tests/integration/BasicHttpClientJenkinsServerTest.java
+++ src/test/java/nl/tudelft/jenkins/tests/integration/BasicHttpClientJenkinsServerTest.java
@@ -3,15 +3,22 @@ package nl.tudelft.jenkins.tests.integration;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BasicHttpClientJenkinsServerTest extends AbstractJenkinsIntegrationTestBase {
+
+	private static final Logger LOG = LoggerFactory.getLogger(BasicHttpClientJenkinsServerTest.class);
 
 	@Override
 	@Before
@@ -25,14 +32,33 @@ public class BasicHttpClientJenkinsServerTest extends AbstractJenkinsIntegration
 
 	}
 
+	@Test
 	public void testThatJenkinsServiceOnLocalhostCanBeReached() throws Exception {
 
+		String url = "http://localhost:8080/jenkins/login";
+
+		LOG.info("Checking URL: {}", url);
+
 		HttpClient client = new DefaultHttpClient();
-		HttpGet get = new HttpGet("http://localhost:8080/jenkins/login");
+		HttpGet get = new HttpGet(url);
 
 		HttpResponse response = client.execute(get);
 
 		assertThat(response.getStatusLine().getStatusCode(), is(HttpStatus.SC_OK));
+
+		Header[] headers = response.getHeaders("X-Jenkins");
+		String version = "";
+		if (headers.length == 0) {
+			Assert.fail("No X-Jenkins header in response.");
+		} else if (headers.length == 1) {
+			version = headers[0].getValue();
+		} else {
+			for (Header header : headers) {
+				version += header.getValue();
+			}
+		}
+
+		LOG.info("Version: {}", version);
 
 	}
 
